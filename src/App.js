efd659715b9478164bd19fff426c4a97123e1f72
RajSolai <msraj085@gmail.com>
2022-07-13 12:22:20 +0530

    init

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+//import BasicExample from './Component/Sidebar.js'
+//import Dashboard from './Component/Dashboard.jsx';
+import {SideBar} from './Component/cdbSidebar'
+// import SideBar from "./Component/Sidebar.jsx"
+function App() {
+  return (
+    <div className="App">
+      <SideBar/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Component/cdbSidebar.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import {
+  CDBSidebar,
+  CDBSidebarContent,
+  CDBSidebarHeader,
+  CDBSidebarMenu,
+  CDBSidebarMenuItem,
+  CDBSidebarFooter,
+} from 'cdbreact';
+import "./Sidebar.css";
+
+export const SideBar = () => {
+  return (
+      <CDBSidebar className="side-bar-full">
+        <CDBSidebarHeader prefix={<i className="fa fa-bars" />}>Contrast</CDBSidebarHeader>
+        <CDBSidebarContent>
+          <CDBSidebarMenu>
+            <CDBSidebarMenuItem icon="th-large">Dashboard</CDBSidebarMenuItem>
+            <CDBSidebarMenuItem icon="sticky-note">Components</CDBSidebarMenuItem>
+            <CDBSidebarMenuItem icon="credit-card" iconType="solid">
+              Metrics
+            </CDBSidebarMenuItem>
+          </CDBSidebarMenu>
+        </CDBSidebarContent>
+
+        <CDBSidebarFooter style={{ textAlign: 'center' }}>
+          <div
+            className="sidebar-btn-wrapper"
+            style={{padding: '20px 5px'}}
+          >
+            Sidebar Footer
+          </div>
+        </CDBSidebarFooter>
+      </CDBSidebar>
+  );
+};
